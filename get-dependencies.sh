b0afc17c5b82b0b6d31ac4535644ba8e12682d67
domesya <163451587+domesya@users.noreply.github.com>
2026-09-02 17:38:23 +0200

    Add x42-plugins-lv2 to package installation

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ pacman -Rdd --noconfirm pulseaudio || true
 pacman -Syu --noconfirm \
   easyeffects \
   pipewire-pulse pipewire-jack pipewire \
-  calf lsp-plugins-lv2 mda.lv2 \
+  calf lsp-plugins-lv2 mda.lv2 x42-plugins-lv2  \
   yelp noto-fonts \
   kvantum qt6ct
 
