f35f7d5ca0e98dad37bc1bc29cc5860d0c88932e
domesya <renski.k2@gmail.com>
2026-09-03 17:03:02 +0200

    huhh

--- make-appimage.sh
+++ make-appimage.sh
@@ -30,7 +30,7 @@ quick-sharun \
   /usr/lib/libzita-convolver.so* \
   /usr/lib/lv2/mda.lv2/Bandisto.so \
   /usr/lib/libKirigamiFormsPrivateCards.so* \
-  /usr/lib/lv2/lsp-plugins.lv2/para_equalizer_x32_ms.so 
+  /usr/lib/lv2/lsp-plugins.lv2/para_equalizer_x32_ms.ttl 
 
 # Additional changes can be done in between here
 
