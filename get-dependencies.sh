36d5fdb5ab1f0d69781d2f9ff007b5920c4ffbf6
domesya <163451587+domesya@users.noreply.github.com>
2026-09-06 10:44:33 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,8 +19,8 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano kiconthemes-mini 
 
 # Comment this out if you need an AUR package
+make-aur-package deepfilternet-plus-git
 make-aur-package zam-plugins-git
-make-aur-package deepfilternet-plus-bin
 # If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
